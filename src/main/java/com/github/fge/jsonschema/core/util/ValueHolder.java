3fc0235cefcc35002337785f98a954de516cfacd
Francis Galiegue <fgaliegue@gmail.com>
2014-03-06 09:54:15 +0100

    ValueHolder: make final
    Ultimate goal is to remove it. This is the first step.

--- src/main/java/com/github/fge/jsonschema/core/util/ValueHolder.java
+++ src/main/java/com/github/fge/jsonschema/core/util/ValueHolder.java
@@ -18,9 +18,6 @@
 
 package com.github.fge.jsonschema.core.util;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.github.fge.jackson.JacksonUtils;
 import com.github.fge.jsonschema.core.processing.Processor;
 import com.github.fge.jsonschema.core.report.MessageProvider;
 import com.github.fge.jsonschema.core.report.ProcessingMessage;
@@ -34,38 +31,34 @@ import javax.annotation.concurrent.Immutable;
  * {@link MessageProvider}, this abstract class helps to wrap values and
  * implement this interface at the same time.</p>
  *
- * <p>Implementations need only implement the {@link #valueAsJson()} method.</p>
- *
  * @param <T> the type of the value
  */
 @Immutable
-public abstract class ValueHolder<T>
+public final class ValueHolder<T>
     implements MessageProvider
 {
-    protected static final JsonNodeFactory FACTORY = JacksonUtils.nodeFactory();
-
     private final String name;
-    protected final T value;
+    private final T value;
 
     public static <V> ValueHolder<V> hold(final V value)
     {
-        return new SimpleValueHolder<V>("value", value);
+        return new ValueHolder<V>("value", value);
     }
 
     public static <V> ValueHolder<V> hold(final String name, final V value)
     {
-        return new SimpleValueHolder<V>(name, value);
+        return new ValueHolder<V>(name, value);
     }
 
     public static <V extends AsJson> ValueHolder<V> hold(final V value)
     {
-        return new AsJsonValueHolder<V>("value", value);
+        return new ValueHolder<V>("value", value);
     }
 
     public static <V extends AsJson> ValueHolder<V> hold(final String name,
         final V value)
     {
-        return new AsJsonValueHolder<V>(name, value);
+        return new ValueHolder<V>(name, value);
     }
 
     /**
@@ -74,25 +67,18 @@ public abstract class ValueHolder<T>
      * @param name the name to prefix the value with
      * @param value the value
      */
-    protected ValueHolder(final String name, final T value)
+    private ValueHolder(final String name, final T value)
     {
         this.name = name;
         this.value = value;
     }
 
     /**
-     * Return a JSON representation of the value
-     *
-     * @return a {@link JsonNode}
-     */
-    protected abstract JsonNode valueAsJson();
-
-    /**
      * Return the declared name for this value
      *
      * @return the declared name
      */
-    public final String getName()
+    public String getName()
     {
         return name;
     }
@@ -102,7 +88,7 @@ public abstract class ValueHolder<T>
      *
      * @return the value
      */
-    public final T getValue()
+    public T getValue()
     {
         return value;
     }
@@ -113,8 +99,8 @@ public abstract class ValueHolder<T>
      * @return a new {@link ProcessingMessage}
      */
     @Override
-    public final ProcessingMessage newMessage()
+    public ProcessingMessage newMessage()
     {
-        return new ProcessingMessage().put(name, valueAsJson());
+        return new ProcessingMessage().put(name, JsonUtils.toJson(value));
     }
 }
